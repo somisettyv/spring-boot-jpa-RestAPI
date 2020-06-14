cd9547a969d1dd98607f7b5fbd94dd1de99d06d3
venky somisetty <venky.somisetty@gmail.com>
2020-06-14 16:52:38 -0400

    no message

--- src/main/java/com/mytech/employee/Application.java
+++ src/main/java/com/mytech/employee/Application.java
@@ -5,8 +5,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.mytech.employee.service.EmplomentService;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class Application {
 	
 	@Autowired
